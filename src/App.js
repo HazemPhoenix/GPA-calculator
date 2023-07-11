846077361caf774d4a5bb26989200497995b6d6b
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2023-07-11 03:11:01 +0300

    finished the app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GPA from "./Components/GPA";
+
+function App() {
+  return (
+    <div>
+      <GPA />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/GPA.jsx
@@ -0,0 +1,97 @@
+import "./GPA_style.css";
+import React from "react";
+import { useState } from "react";
+
+function GPA() {
+  const [semester, setSemester] = useState(1);
+  const [prevGPA, setprevGPA] = useState(0);
+  const [semGPA, setsemGPA] = useState(0);
+  const [GPA, setGPA] = useState(0);
+  const credits = [19, 19, 19, 19, 18, 19, 17, 16];
+  const calcGPA = (prevGPA, semGPA, semester) => {
+    let totalCredits = credits
+      .slice(0, semester - 1)
+      .reduce((a, b) => a + b, 0);
+    return (
+      (prevGPA * totalCredits + semGPA * credits[semester - 1]) /
+      (totalCredits + credits[semester - 1])
+    );
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setGPA(Math.round(calcGPA(prevGPA, semGPA, semester) * 1000) / 1000);
+  };
+  const reset = () => {
+    setSemester(1);
+    setprevGPA(0);
+    setsemGPA(0);
+    setGPA(0);
+  };
+
+  const subjects = {
+    "semester 1": [4, 4, 4, 3, 2, 2],
+    "Semester 2": [4, 4, 4, 3, 2, 2],
+    "Semester 3": [4, 4, 4, 4, 3],
+    "Semester 4": [5, 4, 4, 2, 3],
+    "Semester 5": [4, 4, 3, 3, 2, 2],
+    "Semester 6": [4, 4, 3, 3, 3, 2],
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={submitHandler}>
+        <label htmlFor="semester">Semester: </label>
+        <select
+          name="semester"
+          id="semester"
+          onChange={(e) => setSemester(e.target.value)}
+          value={semester}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+        </select>
+        <br />
+        <label htmlFor="prevGPA">Previous GPA: </label>
+        <input
+          type="number"
+          name="prevGPA"
+          id="prevGPA"
+          min={0}
+          max={5}
+          step={0.001}
+          onChange={(e) => setprevGPA(e.target.value)}
+          value={prevGPA}
+        />
+        <br />
+        <label htmlFor="semGPA">Semester GPA: </label>
+        <input
+          type="number"
+          name="semGPA"
+          id="semGPA"
+          min={0}
+          max={5}
+          step={0.001}
+          onChange={(e) => setsemGPA(e.target.value)}
+          value={semGPA}
+        />
+        <br />
+        <button type="submit">Calculate</button>
+        <button type="button" onClick={reset}>
+          Reset
+        </button>
+      </form>
+      <p>
+        Cumulative GPA: <span className="GPA">{GPA}</span>
+      </p>
+    </div>
+  );
+}
+
+export default GPA;
